f8694cf950bc71f8e1a7d0f230321d757fd7aae8
Luis Juarez <luisisthejuarez@gmail.com>
2020-11-30 21:21:52 -1000

    added signin screen

---
+++ src/SigninScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text>Sign in here</Text>
+    </View>
+  );
+};
+
+export default SigninScreen;

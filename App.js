6bd4c2ac806986f6e455aff1c3c032bab4c220d1
Luis Juarez <luisisthejuarez@gmail.com>
2020-11-30 21:50:13 -1000

    set up navigation flow

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import SpotCreateScreen from "./src/screens/SpotCreateScreen";
+import SpotDetailScreen from "./src/screens/SpotDetailScreen";
+import SpotListScreen from "./src/screens/SpotListScreen";
 
-export default function App() {
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signin: SigninScreen,
+    SignUp: SignupScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    spotListFlow: createStackNavigator({
+      SpotList: SpotListScreen,
+      SpotDetail: SpotDetailScreen,
+    }),
+    Account: AccountScreen,
+    SpotCreate: SpotCreateScreen,
+  }),
+});
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Placeholder while I setup navigation</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +43,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default App;

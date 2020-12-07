038a1c7459b3d8441a078e5b6711a77b33d38081
Luis Juarez <luisisthejuarez@gmail.com>
2020-12-06 16:35:50 -1000

    fix: map component now pulls users current location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,16 @@
-import { requestPermissionsAsync } from "expo-location";
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
-  const [userLocation, setUserLocation] = useState();
+  const [userLocation, setUserLocation] = useState(null);
+  const [locationError, setlocationError] = useState("");
 
   const points = [];
   for (let i = 0; i < 15; i++) {
@@ -18,16 +24,31 @@ const Map = () => {
     try {
       const { granted } = await requestPermissionsAsync;
       if (!granted) {
-        console.log("Permission not grante");
+        console.log("Permission not granted");
       } else {
         console.log("Permission granted");
+        await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          (location) => {
+            console.log(location);
+          }
+        );
       }
+      let currentLocation = await getCurrentPositionAsync();
+      setUserLocation(currentLocation);
     } catch (err) {
+      setlocationError(err);
       console.log(err);
     }
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    startWatchingLocation();
+  }, []);
 
   return (
     <View>
@@ -35,6 +56,8 @@ const Map = () => {
       <MapView style={styles.map}>
         <Polyline coordinates={points} />
       </MapView>
+      {locationError ? <Text>Please turn on Location Services</Text> : null}
+      {userLocation ? <Text>{userLocation}</Text> : null}
     </View>
   );
 };
